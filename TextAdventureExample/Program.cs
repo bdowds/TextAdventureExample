3bbea053602b81a52659988d3e0000f42410adcf
bwdowds <bwdowds@gmail.com>
2019-07-12 19:27:09 -0500

    Add project files.

---
+++ TextAdventureExample/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TextAdventureExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
